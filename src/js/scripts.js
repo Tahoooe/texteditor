a2f67d016cc58f53443e98fc37458c5ee4754288
Tahoe <50768582+Tahoooe@users.noreply.github.com>
2019-11-01 20:14:05 +0100

    Des settings qui font décoration

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,7 +8,10 @@ document.addEventListener('mousemove', e => {
   	light.style.left = e.pageX + "px"  
 })
 
+
+
 const editor = document.getElementById("editor");
+
 
 function mkHeader() {
 	document.execCommand('formatBlock',false,'h1');
@@ -92,9 +95,9 @@ document.addEventListener("keydown", function(key) {
 		key.preventDefault();
 		mkAlignLeft(); // cmd/ctrl + l
 
-	} else if ((window.navigator.platform.match("Mac") ? key.metaKey : key.ctrlKey)  && key.keyCode == 82) {
-		key.preventDefault();
-		mkAlignRight(); // cmd/ctrl + r
+	// } else if ((window.navigator.platform.match("Mac") ? key.metaKey : key.ctrlKey)  && key.keyCode == 82) {
+	// 	key.preventDefault();
+	// 	mkAlignRight(); // cmd/ctrl + r
 
 	} else if ((window.navigator.platform.match("Mac") ? key.metaKey : key.ctrlKey) && event.altKey && key.keyCode == 81) {
 		key.preventDefault();
@@ -143,10 +146,121 @@ editor.addEventListener("input", function() {
 }, false);
 
 
-function fullScreen() { 
-	if (document.fullscreenElement) { 
-		document.exitFullscreen() 
-	} else { 
-		document.documentElement.requestFullscreen() 
-	} 
-};
+// function checkFullscreen() {
+// 	if (window.fullscreen) {
+// 		fullscreenButton.classList.add('fa-close');
+// 		fullscreenButton.classList.remove('fa-expand');
+// 	} else {
+// 		fullscreenButton.classList.remove('fa-close');
+// 		fullscreenButton.classList.add('fa-expand');
+// 	}
+// }
+// checkFullscreen();
+
+
+
+//joli toggle 
+function toggle(toggling, target) {
+	let parent = target.parentElement;
+	parent.classList.remove("tgLeft", "tgCenter", "tgRight");
+
+	switch (toggling) {
+		case "left":
+			parent.classList.add("tgLeft");
+			break;
+		case "center":
+			parent.classList.add("tgCenter");
+			break;
+		case "right":
+			parent.classList.add("tgRight");
+			break;
+	}
+}
+
+function initToggle() {
+	document.getElementById("togglingTwoCtn").classList.add("tgLeft");
+	document.getElementById("togglingThreeCtn").classList.add("tgLeft");
+}
+initToggle();
+
+
+
+
+
+
+
+
+var x, i, j, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < selElmnt.length; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect); 
